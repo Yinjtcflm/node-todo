aa5ac7871a94200db5ae8b943f898656a3347bec
Yinjtcflm <1529792769@163.com>
2022-09-27 23:16:14 +0800

    修改写功能不能成功的bug

--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -1,22 +1,25 @@
-const db=require('../db.js')
-const fs=require('fs')
-jest.mock('fs')
+const db = require("../db.js");
+const fs = require("fs");
+jest.mock("fs");
 
-describe('db',()=>{
-    it('can read',async ()=>{
-        const data=[{title:'hi',done:true}]
-        fs.setReadMock('/xxx',null,JSON.stringify(data))
-        const list =await db.read('/xxx')
-        expect(list).toStrictEqual(data)
-    })
-    it('can write',async()=>{
-        let fakeFile;
-        fs.setWriteMock('/yyy',(path,data,callback)=>{
-            fakeFile=data;
-            callback(null)
-        })
-        const list =[{title:'见朴灿烈',done:true},{title:'见边伯贤',done:true}]
-        await db.write(list,'/yyy')
-        expect(fakeFile).toBe(JSON.stringify(list)+'\n')
-    })
-})
+describe("db", () => {
+  it("can read", async () => {
+    const data = [{ title: "hi", done: true }];
+    fs.setReadMock("/xxx", null, JSON.stringify(data));
+    const list = await db.read("/xxx");
+    expect(list).toStrictEqual(data);
+  });
+  it("can write", async () => {
+    let fakeFile;
+    fs.setWriteMock("/yyy", (path, data, callback) => {
+      fakeFile = data;
+      callback(null);
+    });
+    const list = [
+      { title: "见朴灿烈", done: true },
+      { title: "见边伯贤", done: true },
+    ];
+    await db.write(list, "/yyy");
+    expect(fakeFile).toBe(JSON.stringify(list) + "\n");
+  });
+});
